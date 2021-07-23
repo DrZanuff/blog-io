76a8d1cb828033ec71a027fb8ead4087de7bb03a
Ricardo <ricardo.machado.nwi@gmail.com>
2021-07-23 09:29:51 -0300

    day 1

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,9 @@
 import { GetStaticProps } from 'next';
-
+import { ReactElement } from 'react';
+import { FiCalendar, FiUser } from 'react-icons/fi';
+import Link from 'next/link';
 import { getPrismicClient } from '../services/prismic';
+
 
 import commonStyles from '../styles/common.module.scss';
 import styles from './home.module.scss';
@@ -24,9 +27,62 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home(): ReactElement {
+  return (
+    <>
+      <header className={styles.heading}>
+        <img src="/images/Logo.svg" alt="logo" />
+      </header>
+
+      <main className={styles.content}>
+
+        <div className={styles.post}>
+          <h1>Como utilizar Hooks</h1>
+          <span className={styles.postTitle}>
+            Pensando em sincronização em vez de ciclos de vida.
+          </span>
+          <footer className={commonStyles.info}>
+            <FiCalendar />
+            <span>15 Mar 2021</span>
+            <FiUser />
+            <span>Joseph Oliveira</span>
+          </footer>
+        </div>
+
+        <div className={styles.post}>
+          <h1>Como utilizar Hooks</h1>
+          <span className={styles.postTitle}>
+            Pensando em sincronização em vez de ciclos de vida.
+          </span>
+          <footer className={commonStyles.info}>
+            <FiCalendar />
+            <span>15 Mar 2021</span>
+            <FiUser />
+            <span>Joseph Oliveira</span>
+          </footer>
+        </div>
+
+        <div className={styles.post}>
+          <h1>Como utilizar Hooks</h1>
+          <span className={styles.postTitle}>
+            Pensando em sincronização em vez de ciclos de vida.
+          </span>
+          <footer className={commonStyles.info}>
+            <FiCalendar />
+            <span>15 Mar 2021</span>
+            <FiUser />
+            <span>Joseph Oliveira</span>
+          </footer>
+        </div>
+
+      </main>
+
+      <footer className={styles.foot}>
+        <button type="button">Carregar mais posts</button>
+      </footer>
+    </>
+  );
+}
 
 // export const getStaticProps = async () => {
 //   // const prismic = getPrismicClient();
